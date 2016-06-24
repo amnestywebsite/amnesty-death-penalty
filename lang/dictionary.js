4ce791a0e2f95d359f8fda3b8dffe755ccab801f
robertocarroll <rob@wearethoughtfox.com>
2016-06-24 10:18:23 +0100

    Added an example translation file for all the bits of text I could find.

---
+++ lang/dictionary.js
@@ -0,0 +1,68 @@
+{
+   "ABOLITIONIST":{
+      "en":"Abolitionist",
+      "es":"Abolitionist-es",
+      "fr":"Abolitionist-fr",
+      "ar":"Abolitionist-ar"
+   },
+   "RETENTIONIST":{
+      "en":"Retentionist",
+      "es":"Retentionist-es",
+      "fr":"Retentionist-fr",
+      "ar":"Retentionist-ar"
+   },
+   "ABOLITIONIST IN PRACTICE":{
+      "en":"Abolitionist in practice",
+      "es":"Abolitionist in practice-es",
+      "fr":"Abolitionist in practice-fr",
+      "ar":"Abolitionist in practice-ar"
+   },
+   "ABOLITIONIST FOR ORDINARY CRIMES":{
+      "en":"Abolitionist for ordinary crimes",
+      "es":"Abolitionist for ordinary crimes-es",
+      "fr":"Abolitionist for ordinary crimes-fr",
+      "ar":"Abolitionist for ordinary crimes-ar"
+   },
+   "ABOLITIONIST DEFINITION":{
+      "en":"Does not use the death penalty",
+      "es":"Does not use the death penalty-es",
+      "fr":"Does not use the death penalty-fr",
+      "ar":"Does not use the death penalty-ar"
+   },
+   "RETENTIONIST DEFINITION":{
+      "en":"Retains the death penalty in law",
+      "es":"Retains the death penalty in law-es",
+      "fr":"Retains the death penalty in law-fr",
+      "ar":"Retains the death penalty in law-ar"
+   },
+   "ABOLITIONIST IN PRACTICE DEFINITION":{
+      "en":"Retains the death penalty in law, but hasn’t executed for at least 10 years",
+      "es":"Retains the death penalty in law, but hasn’t executed for at least 10 years-es",
+      "fr":"Retains the death penalty in law, but hasn’t executed for at least 10 years-fr",
+      "ar":"Retains the death penalty in law, but hasn’t executed for at least 10 years-ar"
+   },
+   "ABOLITIONIST FOR ORDINARY CRIMES DEFINITION":{
+      "en":"Retains the death penalty only for exceptional crimes, such as those committed during times of war",
+      "es":"Retains the death penalty only for exceptional crimes, such as those committed during times of war-es",
+      "fr":"Retains the death penalty only for exceptional crimes, such as those committed during times of war-fr",
+      "ar":"Retains the death penalty only for exceptional crimes, such as those committed during times of war-ar"
+   },
+   "EXECUTIONS":{
+      "en":"Executions",
+      "es":"Executions-es",
+      "fr":"Executions-fr",
+      "ar":"Executions-ar"
+   },
+   "DEATH SENTENCES":{
+      "en":"Death sentences",
+      "es":"Death sentences-es",
+      "fr":"Death sentences-fr",
+      "ar":"Death sentences-ar"
+   },
+   "SINCE":{
+      "en":"Since",
+      "es":"Since-es",
+      "fr":"Since-fr",
+      "ar":"Since-ar"
+   }
+}
