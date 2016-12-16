62d03fa43aa9a45cb4d71890c2d1f86e5055e801
Paul D. Waite <paul@pauldwaite.co.uk>
2016-12-16 12:04:26 +0000

    Added code to redraw slider with updated width on window resize, and changed slider button/timeline layout to avoid the timeline wrapping onto another line occasionally. See #58.

--- javascripts/amnesty.js
+++ javascripts/amnesty.js
@@ -284,7 +284,6 @@ function draw(topo, activeCountries, coastline, somalilandBorder, kosovoBorder, 
   new Awesomplete(document.querySelector('.awesomplete'));
   document.querySelector('.awesomplete').addEventListener('awesomplete-selectcomplete', function (e) {
     var selectedCountryName = e.text.value;
-    console.log (selectedCountryName);
 
     var selectedCountryId = countryIdByName[selectedCountryName];
 
@@ -775,6 +774,18 @@ function setupSlider() {
     });
 }
 
+function updateSliderWidth() {
+  var newSliderWidth = windowWidth - sliderPlayPauseButton.getBoundingClientRect().width,
+      existingSliderSVGEl = document.querySelector('.chroniton .slider');
+
+  // Removing existing slider pointer element, to avoid it being redrawn repeatedly for some reason
+  existingSliderSVGEl.parentNode.removeChild(existingSliderSVGEl);
+
+  customSlider.width(newSliderWidth);
+
+  d3.select("#slider").call(customSlider);
+}
+
 function setupNextPreviousYear() {
   document.getElementById('timeline-previous').addEventListener('click', function () {
     var newYear;
@@ -882,6 +893,7 @@ function throttle() {
   window.clearTimeout(throttleTimer);
     throttleTimer = window.setTimeout(function() {
       redraw();
+      updateSliderWidth();
       pymChild.sendHeight();
     }, 200);
 }
